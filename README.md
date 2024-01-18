630abe7b3028f98315d1e500fab08040e762fc48
cvquez <cristhianvelazquez@fctunca.edu.py>
2024-01-18 20:12:08 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## How to use
+
+```sh
+cd maqueta/
+yarn install
+yarn start
+```
---
+++ assets/js/untitled.js
@@ -0,0 +1,6 @@
+$(document).ready(()=>{
+   $(".form-select").select2({
+     theme: "bootstrap-5",
+   });
+   $(".your-class").slick();
+});
